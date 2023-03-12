f739357b6b6db117b488a5014a6e705aa41e5493
MHL <mhl@theabsolute.plus>
2023-03-12 16:00:32 -0700

    Build using tsup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Plugin } from 'vite'
 
-import { compile, CompileOptions } from 'prql-js'
+import prqljs from 'prql-js'
 export { CompileOptions } from 'prql-js'
 
 import * as pl from 'parse-literals'
@@ -12,19 +12,25 @@ export function prql(s: TemplateStringsArray, ...rest: string[]): string {
   return s.map((str, i) => str + (rest[i] ?? '')).join('')
 }
 
-function toModuleCode(s: string, compileOptions?: CompileOptions): string {
-  const sql = compile(s, compileOptions)
+function toModuleCode(
+  s: string,
+  compileOptions?: prqljs.CompileOptions
+): string {
+  const sql = prqljs.compile(s, compileOptions)
   return `export default ${toSource(sql)}`
 }
 
-function toString(prql: string, compileOptions?: CompileOptions): string {
-  return compile(prql, compileOptions) || ''
+function toString(
+  prql: string,
+  compileOptions?: prqljs.CompileOptions
+): string {
+  return prqljs.compile(prql, compileOptions) || ''
 }
 
 const fileRegex = /\.(prql)$/
 
 export type PrqlPluginConfig = {
-  compileOptions?: CompileOptions
+  compileOptions?: prqljs.CompileOptions
 }
 
 export default function prqlPlugin(config: PrqlPluginConfig = {}): Plugin {
